3b0cd875c5e7a9b209046509c1c0fffd587068d7
Nna <llina.tvo@gmail.com>
2019-11-07 17:50:25 +0100

    Adding files.

---
+++ src/robots.js
@@ -0,0 +1,44 @@
+export const robots = [
+  {
+    id: 1,
+    name: "Na",
+    username: "Nna",
+    email: "Nna@gmail.com"
+  },
+  {
+    id: 2,
+    name: "Boni",
+    username: "Bboni",
+    email: "Boni@gmail.com"
+  },
+  {
+    id: 3,
+    name: "Fido",
+    username: "Ffido",
+    email: "Ffido@gmail.com"
+  },
+  {
+    id: 4,
+    name: "Bubu",
+    username: "Bbubu",
+    email: "Bubu@gmail.com"
+  },
+  {
+    id: 5,
+    name: "Bim",
+    username: "Bbim",
+    email: "Bim@gmail.com"
+  },
+  {
+    id: 6,
+    name: "Bam",
+    username: "Bbam",
+    email: "Bam@gmail.com"
+  },
+  {
+    id: 7,
+    name: "Heo",
+    username: "Hheo",
+    email: "Heo@gmail.com"
+  }
+];
